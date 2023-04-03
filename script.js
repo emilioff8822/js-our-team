3f5db2804bdb7f580a27fb0d3e8addada0513fb5
emilioff8822 <emilioff@hotmail.it>
2023-04-03 15:44:10 +0200

    comm-array-img

---
+++ script.js
@@ -0,0 +1,32 @@
+const team = [
+  {
+    nome: "wayne barnett",
+    ruolo: "founder & ceo",
+    immagine: "wayne-barnett-founder-ceo.jpg"
+  },
+  {
+    nome: "angela caroll",
+    ruolo: "chief editor",
+    immagine: "angela-caroll-chief-editor.jpg"
+  },
+  {
+    nome: "walter gordon",
+    ruolo: "office manager",
+    immagine: "walter-gordon-office-manager.jpg"
+  },
+  {
+    nome: "angela lopez",
+    ruolo: "social media manager",
+    immagine: "angela-lopez-social-media-manager.jpg"
+  },
+  {
+    nome: "scott estrada",
+    ruolo: "developer",
+    immagine: "scott-estrada-developer.jpg"
+  },
+  {
+    nome: "barbara ramos",
+    ruolo: "graphic designer",
+    immagine: "barbara-ramos-graphic-designer.jpg"
+  }
+];
